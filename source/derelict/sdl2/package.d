e1471bbd348b61375af5aa0ab15411115012992a
Mike Parker <aldacron@gmail.com>
2013-12-15 22:07:41 +0900

    Added package.d

---
+++ source/derelict/sdl2/package.d
@@ -0,0 +1,7 @@
+module package.d;
+
+public import derelict.sdl2.image,
+              derelict.sdl2.mixer,
+              derelict.sdl2.net,
+              derelict.sdl2.sdl,
+              derelict.sdl2.ttf;
