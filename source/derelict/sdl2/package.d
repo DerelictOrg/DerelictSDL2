595e5f6191a67c1313e19ef5e0439af676bd7559
Mike Parker <aldacron@gmail.com>
2013-12-22 18:36:46 +0900

    Removed package.d

--- source/derelict/sdl2/package.d
+++
@@ -1,7 +0,0 @@
-module derelict.sdl2;
-
-public import derelict.sdl2.image,
-              derelict.sdl2.mixer,
-              derelict.sdl2.net,
-              derelict.sdl2.sdl,
-              derelict.sdl2.ttf;
