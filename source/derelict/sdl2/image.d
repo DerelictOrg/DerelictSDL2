e11e52a0f920adb183bbcf57035198bd47ef777a
Mike Parker <aldacron@gmail.com>
2016-11-04 14:49:11 +0900

    Support for statically binding SDL_mixer

--- source/derelict/sdl2/image.d
+++ source/derelict/sdl2/image.d
@@ -30,7 +30,6 @@ module derelict.sdl2.image;
 import derelict.sdl2.types;
 
 version(Derelict_Static) version = DerelictSDL2_Static;
-
 version(DerelictSDL2_Static)
     import derelict.sdl2.sdlstatic : SDL_GetError, SDL_SetError;
 else
@@ -39,29 +38,71 @@ else
 alias IMG_SetError = SDL_SetError;
 alias IMG_GetError = SDL_GetError;
 
+enum : Uint8 {
+    SDL_IMAGE_MAJOR_VERSION     = 2,
+    SDL_IMAGE_MINOR_VERSION     = 0,
+    SDL_IMAGE_PATCHLEVEL        = 0,
+}
+
+@nogc nothrow void SDL_IMAGE_VERSION(SDL_version* X) {
+    X.major     = SDL_IMAGE_MAJOR_VERSION;
+    X.minor     = SDL_IMAGE_MINOR_VERSION;
+    X.patch     = SDL_IMAGE_PATCHLEVEL;
+}
+
+enum {
+    IMG_INIT_JPG    = 0x00000001,
+    IMG_INIT_PNG    = 0x00000002,
+    IMG_INIT_TIF    = 0x00000004,
+    IMG_INIT_WEBP   = 0x00000008,
+}
+
 version(DerelictSDL2_Static) {
+    extern(System) @nogc nothrow {
+        int IMG_Init(int);
+        int IMG_Quit();
+        const(SDL_version)* IMG_Linked_Version();
+        SDL_Surface* IMG_LoadTyped_RW(SDL_RWops*,int,const(char)*);
+        SDL_Surface* IMG_Load(const(char)*);
+        SDL_Surface* IMG_Load_RW(SDL_RWops*,int);
+        SDL_Texture* IMG_LoadTexture(SDL_Renderer*,const(char)*);
+        SDL_Texture* IMG_LoadTexture_RW(SDL_Renderer*,SDL_RWops*,int);
+        SDL_Texture* IMG_LoadTextureTyped_RW(SDL_Renderer*,SDL_RWops*,int,const(char)*);
+        int IMG_isICO(SDL_RWops*);
+        int IMG_isCUR(SDL_RWops*);
+        int IMG_isBMP(SDL_RWops*);
+        int IMG_isGIF(SDL_RWops*);
+        int IMG_isJPG(SDL_RWops*);
+        int IMG_isLBM(SDL_RWops*);
+        int IMG_isPCX(SDL_RWops*);
+        int IMG_isPNG(SDL_RWops*);
+        int IMG_isPNM(SDL_RWops*);
+        int IMG_isTIF(SDL_RWops*);
+        int IMG_isXCF(SDL_RWops*);
+        int IMG_isXPM(SDL_RWops*);
+        int IMG_isXV(SDL_RWops*);
+        int IMG_isWEBP(SDL_RWops*);
+        SDL_Surface* IMG_LoadICO_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadCUR_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadBMP_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadGIF_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadJPG_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadLBM_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadPCX_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadPNG_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadPNM_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadTGA_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadTIF_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadXCF_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadXPM_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadXV_RW(SDL_RWops*);
+        SDL_Surface* IMG_LoadWEBP_RW(SDL_RWops*);
+    }
+}
+else {
     import derelict.util.loader,
            derelict.util.exception,
            derelict.util.system;
-
-    enum : Uint8 {
-        SDL_IMAGE_MAJOR_VERSION     = 2,
-        SDL_IMAGE_MINOR_VERSION     = 0,
-        SDL_IMAGE_PATCHLEVEL        = 0,
-    }
-
-    @nogc nothrow void SDL_IMAGE_VERSION(SDL_version* X) {
-        X.major     = SDL_IMAGE_MAJOR_VERSION;
-        X.minor     = SDL_IMAGE_MINOR_VERSION;
-        X.patch     = SDL_IMAGE_PATCHLEVEL;
-    }
-
-    enum {
-        IMG_INIT_JPG    = 0x00000001,
-        IMG_INIT_PNG    = 0x00000002,
-        IMG_INIT_TIF    = 0x00000004,
-        IMG_INIT_WEBP   = 0x00000008,
-    }
 
     extern(C) @nogc nothrow {
         alias da_IMG_Init = int function(int);
@@ -224,46 +265,4 @@ version(DerelictSDL2_Static) {
             enum libNames = "libSDL2_image.so,libSDL2_image-2.0.so,libSDL2_image-2.0.so.0,/usr/local/lib/libSDL2_image.so,/usr/local/lib/libSDL2_image-2.0.so,/usr/local/lib/libSDL2_image-2.0.so.0";
         else
             static assert(0,"Need to implement SDL2_image libNames for this operating system.");
-}
-else {
-    extern(System) @nogc nothrow {
-        int IMG_Init(int);
-        int IMG_Quit();
-        const(SDL_version)* IMG_Linked_Version();
-        SDL_Surface* IMG_LoadTyped_RW(SDL_RWops*,int,const(char)*);
-        SDL_Surface* IMG_Load(const(char)*);
-        SDL_Surface* IMG_Load_RW(SDL_RWops*,int);
-        SDL_Texture* IMG_LoadTexture(SDL_Renderer*,const(char)*);
-        SDL_Texture* IMG_LoadTexture_RW(SDL_Renderer*,SDL_RWops*,int);
-        SDL_Texture* IMG_LoadTextureTyped_RW(SDL_Renderer*,SDL_RWops*,int,const(char)*);
-        int IMG_isICO(SDL_RWops*);
-        int IMG_isCUR(SDL_RWops*);
-        int IMG_isBMP(SDL_RWops*);
-        int IMG_isGIF(SDL_RWops*);
-        int IMG_isJPG(SDL_RWops*);
-        int IMG_isLBM(SDL_RWops*);
-        int IMG_isPCX(SDL_RWops*);
-        int IMG_isPNG(SDL_RWops*);
-        int IMG_isPNM(SDL_RWops*);
-        int IMG_isTIF(SDL_RWops*);
-        int IMG_isXCF(SDL_RWops*);
-        int IMG_isXPM(SDL_RWops*);
-        int IMG_isXV(SDL_RWops*);
-        int IMG_isWEBP(SDL_RWops*);
-        SDL_Surface* IMG_LoadICO_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadCUR_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadBMP_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadGIF_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadJPG_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadLBM_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadPCX_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadPNG_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadPNM_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadTGA_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadTIF_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadXCF_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadXPM_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadXV_RW(SDL_RWops*);
-        SDL_Surface* IMG_LoadWEBP_RW(SDL_RWops*);
-    }
 }
